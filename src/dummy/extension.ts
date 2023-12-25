0974014c02a035768fd46e41029747e2d74746e2
송현주 <shyunju7@naver.com>
2023-12-25 23:28:00 +0900

    feat : 기본 데이터셋 구조 변경 및 타입 설정

--- src/dummy/extension.ts
+++ src/dummy/extension.ts
@@ -1,2 +1,45 @@
 /** 기본 확장자 목록 정의 */
-export const extensions = ["bat", "cmd", "com", "cpl", "exe", "scr", "js"];
+export const extensions: ExtensionDataSet = [
+	{
+		id: "bat",
+		name: "bat",
+		isChecked: false,
+		category: "D",
+	},
+	{
+		id: "cmd",
+		name: "cmd",
+		isChecked: false,
+		category: "D",
+	},
+	{
+		id: "com",
+		name: "com",
+		isChecked: false,
+		category: "D",
+	},
+	{
+		id: "cpl",
+		name: "cpl",
+		isChecked: false,
+		category: "D",
+	},
+	{
+		id: "exe",
+		name: "exe",
+		isChecked: false,
+		category: "D",
+	},
+	{
+		id: "scr",
+		name: "scr",
+		isChecked: false,
+		category: "D",
+	},
+	{
+		id: "js",
+		name: "js",
+		isChecked: false,
+		category: "D",
+	},
+];
--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,10 @@
 /// <reference types="vite/client" />
+
+type ExtensionData = {
+	id: string;
+	name: string;
+	isChecked: boolean;
+	category: "D" | "C";
+};
+
+type ExtensionDataSet = ExtensionData[];
